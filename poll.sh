f754eaee186e7016a7a7272e3e4a7a1ef6f61ed9
timmygee <tim@wholebaked.com.au>
2016-05-17 20:20:45 -0400

    Remove superfluous slash

--- poll.sh
+++ poll.sh
@@ -2,6 +2,6 @@
 
 source $HOME/.virtualenvs/investorservitude/bin/activate
 
-$VIRTUAL_ENV/bin/python /$HOME/investorservitude/investorservitude/manage.py poll 2>&1 >> $HOME/poll.log
+$VIRTUAL_ENV/bin/python $HOME/investorservitude/investorservitude/manage.py poll 2>&1 >> $HOME/poll.log
 
 deactivate
